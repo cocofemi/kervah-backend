a8c60fd120bf1c92db297266d4a18a746306f697
femi <cocofemi@gmail.com>
2025-12-01 13:42:54 +0100

    changes

--- src/graphql/business/business.resolver.ts
+++ src/graphql/business/business.resolver.ts
@@ -1,4 +1,4 @@
-import mongoose, { Types } from "mongoose"
+import { Types } from "mongoose"
 import { Business } from "../../models/business.model";
 import { User } from "../../models/user.model";
 import { IBusiness} from "../../interfaces/business.types";
